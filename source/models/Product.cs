bcb846c955f04ba12ceef208a48219e0ceede398
HiroshiGamma <thenarux200@gmai.com>
2024-09-25 09:29:00 -0300

    feat: added models in project

---
+++ source/models/Product.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api01.source.models
+{
+    public class Product
+    {
+        public String name { get; set;} - string.Empty;
+
+        public int Id { get; set; }
+
+        public int Price { get; set; }
+    }
+}
---
+++ source/models/Rol.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api01.source.models
+{
+    public class Rol
+    {
+        
+    }
+}
